3e6e31dfc8ba50fd8bd1693bb790e5f3f38dacbf
Rabin-web-dev <rabin.pvtltd@gmail.com>
2025-09-04 01:05:25 +0530

    Add backend url

--- Frontend/src/services/authService.js
+++ Frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/auth";
+const API_URL = "https://stark-socialmedia.onrender.com/api/auth";
 
 export const signup = async (userData) => {
   return await axios.post(`${API_URL}/signup`, userData);
