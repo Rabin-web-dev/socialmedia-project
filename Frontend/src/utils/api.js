673a35908915647ec6b27d3d2b2d568e4643daf4
Rabin-web-dev <rabin.pvtltd@gmail.com>
2025-09-23 18:27:25 +0530

    Make change in api.js

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,7 +1,16 @@
 import axios from "axios";
 
+const isDev = import.meta.env.MODE === "development";
+
+// const api = axios.create({
+// baseURL: import.meta.env.VITE_API_URL || "https://stark-socialmedia.onrender.com/api",
+//   withCredentials: true,
+// });
+
 const api = axios.create({
-baseURL: import.meta.env.VITE_API_URL || "https://stark-socialmedia.onrender.com/api",
+  baseURL: isDev
+    ? "http://localhost:5000/api" 
+    : import.meta.env.VITE_API_URL, 
   withCredentials: true,
 });
 
